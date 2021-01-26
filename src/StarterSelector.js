96f726d16a4a1d32fc68415057b1bc5c624bb637
Admin <tarik@gmail.com>
2021-01-25 20:25:21 -0500

    solution exercice sur forms

--- src/StarterSelector.js
+++ src/StarterSelector.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+
 function StarterSelector(props){
 
     return (
-        <div>Sélection ici</div>
+        <form>
+            Pick your favorite pokemon:
+
+                <select onChange = {props.pokFun}>
+                    <option value= '0' >{props.pokLst[0]}</option>
+                    <option value= '1'>{props.pokLst[1]}</option>
+                    <option value= '2'>{props.pokLst[2]}</option>
+                </select>
+        </form>
     )
 }
 
